a3cfbb3995b2152082af25da579d8f6785f7b996
Leo Gancedo <gancedoleonel@gmail.com>
2024-01-10 18:12:50 -0300

    Formato de hora

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,14 +1,33 @@
-const APIKEY = '?api_key=sDzcmPCllOQJdhFg1x68MUeiIqbI1XI6hJ28AXXP'
-// const API = 'https://api.nasa.gov/planetary/apod'
-const API = 'https://api.nasa.gov/planetary/apod'
-const contentId = null || document.getElementById('content')
+// API
+const APIKEY = '?api_key=sDzcmPCllOQJdhFg1x68MUeiIqbI1XI6hJ28AXXP';
+const API = 'https://api.nasa.gov/planetary/apod';
 
-const year = "2024"
-const month = "01"
-const day = "01"
-const date = `${year}-${month}-${day}`
+//ELEMENTOS
+const contentId = document.getElementById('content');
 
+//FECHAS Y FORMATO
+const dateFormat = (newDate) => {
+    const newDateToFormat = new Date(newDate)
+    const year = newDateToFormat.getFullYear().toString()
+    const monthError = (newDateToFormat.getMonth()+1).toString()
+    const month = monthError.length === 1 ? `0${monthError}` : monthError
+    const dayError = newDateToFormat.getDate().toString()
+    const day = dayError.length === 1 ? `0${dayError}` : dayError
+    const date = `${year}-${month}-${day}`
+    return date
+}
 
+let dateHoyPre = new Date().getTime()
+const diasResta = 5 * 24 * 60 * 60 * 1000
+let dateInitialPre = (dateHoyPre - diasResta)
+
+const initialDay = dateFormat(dateInitialPre)
+const toDay = dateFormat(dateHoyPre)
+
+console.log(initialDay);
+console.log(toDay);
+
+//FETCH DATA
 const fetchData = async (urlAPI) => {
     const response = await fetch(urlAPI)
     const data = await response.json()
@@ -17,28 +36,27 @@ const fetchData = async (urlAPI) => {
 
 (async () => {
     try {
-        const severalDays = await fetchData(`${API}${APIKEY}&start_date=2024-01-03&end_date=2024-01-07&thumbs=true`)
+        const severalDays = await fetchData(`${API}${APIKEY}&start_date=${initialDay}&end_date=${toDay}&thumbs=true`)
         // const oneDay = await fetchData(`${API}${APIKEY}&date=${date}`)
-        let view = `
-        ${severalDays.map(el => `
-            <div class="group relative">
-                <div
-                    class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                    <img src="${el.thumbnail_url ? el.thumbnail_url : el.url}" alt="" class="w-full">
-                </div>
-                <div class="mt-4 flex justify-between">
-                    <h3 class="text-sm text-gray-700">
-                        <span aria-hidden="true" class="absolute inset-0"></span>
-                        ${el.title}
-                    </h3>
-                </div>
-             </div>
-        `)}
-        `;
+        // let view = `
+        // ${severalDays.map(el => `
+        //     <div class="group relative">
+        //         <div
+        //             class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+        //             <img src="${el.thumbnail_url ? el.thumbnail_url : el.url}" alt="" class="w-full">
+        //         </div>
+        //         <div class="mt-4 flex justify-between">
+        //             <h3 class="text-sm text-gray-700">
+        //                 <span aria-hidden="true" class="absolute inset-0"></span>
+        //                 ${el.title}
+        //             </h3>
+        //         </div>
+        //      </div>
+        // `)}
+        // `;
 
         contentId.innerHTML = view
     } catch (error) {
         console.error(error);
     }
 })();
-
