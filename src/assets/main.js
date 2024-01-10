d215dd5ace5494bd623aaefea9618ca08b237704
Leo Gancedo <gancedoleonel@gmail.com>
2024-01-10 18:56:00 -0300

    Correccion de errores en display de fotos

--- src/assets/main.js
+++ src/assets/main.js
@@ -18,7 +18,7 @@ const dateFormat = (newDate) => {
 }
 
 let dateHoyPre = new Date().getTime()
-const diasResta = 5 * 24 * 60 * 60 * 1000
+const diasResta = 7 * 24 * 60 * 60 * 1000
 let dateInitialPre = (dateHoyPre - diasResta)
 
 const initialDay = dateFormat(dateInitialPre)
@@ -37,24 +37,25 @@ const fetchData = async (urlAPI) => {
 (async () => {
     try {
         const severalDays = await fetchData(`${API}${APIKEY}&start_date=${initialDay}&end_date=${toDay}&thumbs=true`)
+        // console.log(severalDays);
         // const oneDay = await fetchData(`${API}${APIKEY}&date=${date}`)
-        // let view = `
-        // ${severalDays.map(el => `
-        //     <div class="group relative">
-        //         <div
-        //             class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-        //             <img src="${el.thumbnail_url ? el.thumbnail_url : el.url}" alt="" class="w-full">
-        //         </div>
-        //         <div class="mt-4 flex justify-between">
-        //             <h3 class="text-sm text-gray-700">
-        //                 <span aria-hidden="true" class="absolute inset-0"></span>
-        //                 ${el.title}
-        //             </h3>
-        //         </div>
-        //      </div>
-        // `)}
-        // `;
-
+ 
+        let view = `
+        ${severalDays.sort((a,b) => new Date(b.date) - new Date(a.date)).map(el => `
+            <div class="group relative">
+                <div
+                    class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                    <img src="${el.thumbnail_url ? el.thumbnail_url : el.url}" alt="" class="w-full">
+                </div>
+                <div class="mt-4 flex justify-between">
+                    <h3 class="text-sm text-gray-700">
+                        <span aria-hidden="true" class="absolute inset-0"></span>
+                        ${el.date} - ${el.title}
+                    </h3>
+                </div>
+             </div>
+        `).join('')}
+        `;
         contentId.innerHTML = view
     } catch (error) {
         console.error(error);
